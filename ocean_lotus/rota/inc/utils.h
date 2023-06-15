ab89f3573ae3b1874ed3a04aa2259fd723898ef1
Jared Stroud <1250113+archcloudlabs@users.noreply.github.com>
2023-06-15 14:37:34 -0400

    Added self-delete

--- ocean_lotus/rota/inc/utils.h
+++ ocean_lotus/rota/inc/utils.h
@@ -1,6 +1,9 @@
 #ifndef UTILS_H_
 #define UTILS_H_
 
+#include <stdbool.h>
+#include <unistd.h>
+
 /**
  * Create a lock file to ensure one instance is running.
  * TODO - but how does this apply to the watch dog instances?
@@ -10,20 +13,11 @@
 void create_lock();
 
 
-
 /**
- * Create chared memory instance
- * @param mem a char pointer that contains the name of the shared memory instance.
- * @return integer value to be used as a file descriptor
- * */
-int shm_create(char *mem);
-
-
-/**
- * Helper function to convert integer value to string value
- * @param num, integer value to convert.
- * @return point to char memory.
- * */
- char *itoa(int num);
+ *
+ * @brief
+ *
+ **/
+bool self_delete(char *fpath);
 
 #endif // UTILS_H_
