74afb230339e1fa29986e7a2064abee47ab6de45
Jared Stroud <1250113+archcloudlabs@users.noreply.github.com>
2023-06-15 10:37:15 -0400

    bashrc/desktop configs to persist

--- ocean_lotus/rota/inc/persistence.h
+++ ocean_lotus/rota/inc/persistence.h
@@ -13,21 +13,44 @@
 
 
 
-/*
-**
+bool nonroot_bashrc_persistence(void);
+
+bool nonroot_desktop_persistence(void);
+
+bool copy_rota_to_userland(void);
+
+/**
+ * @brief wrapper function to call additional non-root persistence functions
+ * @param N/A
+ * @return boolean value indicating success or failureof non-root persistence methods.
 */
 bool nonroot_persistence(void);
 
-/*
-**
-*/
+/**
+ * @brief Leverage systemd/init rc scripts to achieve persistence when rota is executed as root.
+ * Technique: Boot or Logon Initiaization Scripts
+ * TID: 1037.004
+ * https://attack.mitre.org/techniques/T1037/
+ * @param N/A
+ * @return boolean value if successfully installed persistence.
+ **/
 bool root_persistence(void);
 
 
+/**
+*
+* @brief monitor /proc/<PID> for existence of a given process.
+* @param pid, integer value corresponding to a given process.
+* @return boolean value indicating file in proc exists or not.
+**/
 bool monitor_proc(int pid);
 
-
+/**
+ * @brief helper function to write data to disk
+ * @param fpath char pointer to location on filepath.
+ * @param data, buffer of data to write to previously specified filepath.
+ * @return boolean value indicating success or failure.
+ * */
 bool write_to_file(char *fpath, char *data);
-
 
 #endif // PERSISTENCE_H_
