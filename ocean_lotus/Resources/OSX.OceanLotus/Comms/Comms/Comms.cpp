000f39ab2b6485664b497e877eb0b1db742be6af
mchan143 <61020544+mchan143@users.noreply.github.com>
2023-06-26 12:35:05 -0400

    more to the Comms skeleton

--- ocean_lotus/Resources/OSX.OceanLotus/Comms/Comms/Comms.cpp
+++ ocean_lotus/Resources/OSX.OceanLotus/Comms/Comms/Comms.cpp
@@ -1,9 +1,44 @@
 #include <iostream>
 #include "Comms.hpp"
 
-void sendRequest(const char * s, const std::vector<unsigned char> data)
+std::string buildGETRequestString(const std::vector<unsigned char> data) {
+    // User-Agent: curl/7.11.3
+    // Accept: */*
+    // Cookie: m_pixel_ratio=... | erp=...
+    return "";
+}
+
+std::string buildPOSTRequestString(const std::vector<unsigned char> data) {
+    // User-Agent: curl 7.64.2
+    // Accept: */*
+    // Content-Length: 355
+    // Content-Type: application/x-www-form-urlencoded
+    return "";
+}
+
+void sendRequest(const char * type, const std::vector<unsigned char> data)
 {
-    std::cout << s << std::endl;
-    std::string test(data.begin(), data.end());
-    std::cout << test << std::endl;
+    std::cout << "Type recieved: " + std::string(type) << std::endl;
+    std::string data_str(data.begin(), data.end());
+    std::cout << "Data: " + data_str << std::endl;
+
+    std::string requestBody;
+    if (strcmp(type, "GET")) {
+        requestBody = buildGETRequestString(data);
+    }
+    else if (strcmp(type, "POST")) {
+        requestBody = buildPOSTRequestString(data);
+    }
+    else {
+        std::cout << "Ignoring: " + std::string(type) << std::endl;
+        return;
+    }
+
+    // create socket
+
+    // send data to socket
+
+    // recv data from socket
+
+    // return the data to caller
 };
--- ocean_lotus/Resources/OSX.OceanLotus/Comms/Comms/Comms.hpp
+++ ocean_lotus/Resources/OSX.OceanLotus/Comms/Comms/Comms.hpp
@@ -3,6 +3,22 @@
 
 #include <vector>
 
-extern "C" void sendRequest(const char *, const std::vector<unsigned char> data);
+/*
+sendRequest
+    About:
+        Sends an HTTP request of type (GET/POST) with contained data
+    Result:
+        void for now - this should return the data structure holding HTTP
+        request responses, unclear what this data type is going to be
+    MITRE ATT&CK Techniques:
+    CTI:
+        https://www.trendmicro.com/en_us/research/18/d/new-macos-backdoor-linked-to-oceanlotus-found.html
+        https://www.trendmicro.com/en_us/research/20/k/new-macos-backdoor-connected-to-oceanlotus-surfaces.html
+        https://www.welivesecurity.com/2019/04/09/oceanlotus-macos-malware-update/
+    References:
+        https://stackoverflow.com/questions/70550983/http-request-using-sockets-on-c
+        https://codereview.stackexchange.com/questions/51270/socket-http-post-request
+*/
+extern "C" void sendRequest(const char * type, const std::vector<unsigned char> data);
 
 #endif
