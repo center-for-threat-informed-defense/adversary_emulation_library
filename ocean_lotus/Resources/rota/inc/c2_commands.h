a5a8c06cb1b07ec7bc77f3b08e982d7a53e212b4
Cat <46054582+cat-alyst@users.noreply.github.com>
2023-08-29 11:23:31 -0600

    Update c2_commands.h

--- ocean_lotus/Resources/rota/inc/c2_commands.h
+++ ocean_lotus/Resources/rota/inc/c2_commands.h
@@ -1,14 +1,26 @@
-/**
- *@file c2_commands.h
- *
- *@bref This file contains prototypes for the c2 command functions
- * implemented by the "RotaJakiro" (rota) Linux malware documented by 360 Netlab
- *
- *@references
- * - https://blog.netlab.360.com/stealth_rotajakiro_backdoor_en/
- * - https://blog.netlab.360.com/rotajakiro_linux_version_of_oceanlotus/
- **/
+/*
+# ---------------------------------------------------------------------------
+# c2_commands.h - This file contains prototypes for the c2 command functions
+#                   implemented by the "RotaJakiro" (rota) Linux malware documented by 360 Netlab
 
+# Copyright 2023 MITRE Engenuity. Approved for public release. Document number CT0005.
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+
+# This project makes use of ATT&CK®
+# ATT&CK Terms of Use - https://attack.mitre.org/resources/terms-of-use/ 
+
+# Resources:
+#   https://blog.netlab.360.com/stealth_rotajakiro_backdoor_en/
+#   https://blog.netlab.360.com/rotajakiro_linux_version_of_oceanlotus/
+
+# Revision History:
+
+# ---------------------------------------------------------------------------
+*/
 #ifndef C2_COMMANDS_H_
 #define C2_COMMANDS_H_
 
